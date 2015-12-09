9831d1391bda1e37f1e37c1aa8478affaaa2a6dc
glacaldas <glaucio.caldas@gmail.com>
2015-12-08 23:40:38 -0200

    Create new file

---
+++ functions.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct {
+  char  *nome;
+  struct Palavrabase *next;
+  struct Palavrafilho *filho;
+  int contador;
+} Palavrabase;
+
+typedef struct PalavraSeguinte{
+    char *nome;
+    struct Palavrafilho *prox;
+    int contador;
+
+}Palavrafilho;
+
+typedef struct Consulta{
+    char *nome;
+    struct Consulta *prox;
+
+} Consulta;
+
+
+void filho(Palavrafilho *base, char arr[]){
+	while(base!= NULL){
+		if(base->nome==arr){       //se achar um filho com o a palavra, adiciona uma ocorrencia e sai da função
+			base->contador++;
+			return 1;
+			}
+		else
+			base=base->prox  ; //continua procurando o mesmo nome
+
+
+
+	}
+	Palavrafilho *filho=malloc(sizeof(Palavrafilho));
+	base=filho;
+	base->nome=arr;
+	base->prox=NULL;
+	base->contador=1;  //coloca nodo com ocorrencia 1
+}
+
+void addpalavraunica(Palavrabase *base, char arr[], char arrnova[]){
+    while(base!=NULL){             //procura uma palavra unica igual a palavra dada
+        if(base->nome==arr){         //se acha
+            base->contador++;             //adiciona 1 ao nuemero de ocorrencias
+            filho(base->filho,arrnova);   //pega a proxima palavra e usa a função de adicionar filho
+            return 1;
+            }
+        else
+            base=base->next;           //continua procurando
+
+    }
+
+	Palavrabase *foop=malloc(sizeof(Palavrabase))  ;//se nao achar, adiciona um nodo
+    foop->nome=arr;
+    foop->contador=1;
+	foop->next=NULL;
+	foop->filho=NULL;
+	base=foop;
+}
