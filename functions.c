4cb792f011d4b05648ae46a31eddb398cf2d14ec
glacaldas <glaucio.caldas@gmail.com>
2015-12-09 03:57:30 -0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -46,7 +46,8 @@ void addpalavraunica(Palavrabase *base, char arr[], char arrnova[]){
     while(base!=NULL){             //procura uma palavra unica igual a palavra dada
         if(base->nome==arr){         //se acha
             base->contador++;             //adiciona 1 ao nuemero de ocorrencias
-            filho(base->filho,arrnova);   //pega a proxima palavra e usa a função de adicionar filho
+            filho(base->filho,arrnova);//pega a proxima palavra e usa a função de adicionar filho
+            mergeSort(&base->filho); //sorta a lista de palavras consequentes
             return 1;
             }
         else
@@ -60,6 +61,7 @@ void addpalavraunica(Palavrabase *base, char arr[], char arrnova[]){
 	foop->next=NULL;
 	foop->filho=NULL;
 	base=foop;
+	filho(base->filho,arrnova);
 }
 
 
@@ -76,13 +78,55 @@ int ocorrencia(struct Palavrabase** tamanho, char arr[]){
             base=base->next;
 
     }
+}
 
 
+void split(Palavrafilho *head, Palavrafilho **front, Palavrafilho **back){
+
+	Palavrafilho * fast = head->prox;
+	Palavrafilho * slow = head;
+
+	while(fast){
+		fast = fast->prox;
+		if(fast){
+			fast = fast->prox;
+			slow = slow->prox;
+		}
+	}
+	*front = head;
+	*back = slow->prox;
+	slow->prox = NULL;
+
+}
+Palavrafilho * merge(Palavrafilho *a, Palavrafilho *b){
+    Palavrafilho *result = NULL;
+    if(a ==  NULL)
+        return b;
+    else if(b == NULL)
+        return a;
 
 
+      if(a->contador <= b->contador){
+         result = a;
 
+         result->prox = merge(a->prox,b);
+      }
+      else {
+        result = b;
 
+        result->prox = merge(a,b->prox);
+      }
+      return result;
+}
+void mergeSort(Palavrafilho **headRef){
+	Palavrafilho * front, *back;
+	Palavrafilho * head  = *headRef;
+	if(head == NULL || head->prox == NULL)
+		return;
 
+	split(head, &front, &back);
+	mergeSort(&front);
+	mergeSort(&back);
 
-
+	*headRef = merge(front, back);
 }
