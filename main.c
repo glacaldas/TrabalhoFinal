d76e89c1c4fe100c43808b3f655ad0fbe668bd10
glacaldas <glaucio.caldas@gmail.com>
2015-12-09 02:34:30 -0200

    Update main.c

--- main.c
+++ main.c
@@ -13,15 +13,18 @@ int main(int argc, char *argv[])
     setlocale(LC_ALL,""); //para imprimir corretamente na tela os caracteres acentuados
     clock_t start, end, elapsed;
     int lineno=0,i,nconsulta=atoi(argv[4]); //pega o numero de palavras seguintes
-
+    int valorA,valorB,valorC;
+    float calculo;
     //char nome_arq[]=argv[1];
     FILE * entrada;
 
 
 	struct Palavrabase *usavel;
-	struct Palavrabase *tamanho[MAX];
-	for(i=0;i<=MAX;i++) //inicializa as macro listas
+	struct Palavrabase** tamanho=malloc(sizeof(MAX));
+	for(i=0;i<=13;i++) //inicializa as macro listas
         tamanho[i]=NULL;
+
+    struct Palavrafilho *cabeca;
 
     char *palavra, *palavranova,linha[1000]; // linhas a serem lidas do arquivo
     char separador[]= {" 0123456789,.&*%\?!;/-'@\"$#=><()][}{:\n\t"};
@@ -54,8 +57,8 @@ int main(int argc, char *argv[])
                 palavranova = strtok (NULL, separador);  //pega a proxima palavra tambem
                 palavranova=tolower(palavra);
 
-                if(strlen(palavra)>MAX+1){
-                    usavel=tamanho[MAX];
+                if(strlen(palavra)>13+1){
+                    usavel=tamanho[13];
                     addpalavraunica(usavel, palavra, palavranova);  //ativa a função de adicionar as palavras maiores que 13 caracteres
                 }
                 else{
@@ -92,6 +95,9 @@ int main(int argc, char *argv[])
     }
     fclose(entrada);
 
+
+    usavel=malloc(sizeof(Palavrabase));
+
     entrada=fopen(argv[3],"w"); //abre o arquivo de saída para escrita
     if(entrada==NULL){   //se nao conseguiu abrir o arquivo
         printf("Erro ao abrir o arquivo %s",argv[3]);
@@ -100,21 +106,27 @@ int main(int argc, char *argv[])
     else{  //arquivo ok
         while(lista!=NULL){  //passa por todas as palavras pedidas na consulta
             palavra=lista->nome;
-            if(strlen(palavra)>MAX+1)
-                    usavel=tamanho[MAX];
+            fprintf(entrada,"Consulta: %s\n",palavra);
+            if(strlen(palavra)>13+1)
+                usavel=tamanho[13];
             else
-                usavel=tamanho[strlen(palavra)-1];  //acha em qual arvore de tamanho esta a palavra de consulta
-            while(usavel!=NULL){         //passa por todas as palavras na arvore
-                if(strcmp(usavel->nome==palavra)){
-                    fprintf(entrada,"Consulta: %s\n",palavra);
-
+                usavel=tamanho[strlen(palavra)];
+            while(usavel!=NULL){
+                if(usavel->nome==palavra){
+                    valorB=usavel->contador;
+                    cabeca=usavel->filho;
+                    for(i=0;i<nconsulta;i++){
+                        valorC=ocorrencia(tamanho,cabeca->nome);
+                        valorA=cabeca->contador;
+                        fprintf(entrada,"Sugestao: %s        %0.9f\n",cabeca->nome,valorA/(sqrt(valorB*valorC)));
+                        cabeca=cabeca->prox;
+                    }
                 }
                 else
                     usavel=usavel->next;
-                if(usavel=NULL)
-                    fprintf(entrada,"Palavra %s não se encontra no arquivo\n",lista->nome);
-            }
 
+            }
+            fprintf(entrada,"\n");
         }
     }
 
